849de0e3b799dcbaa1b1d9e67b46dec5ce9d6bf7
Antonio <anthonymartz17@hotmail.com>
2021-08-22 11:03:12 -0400

    initial commit to personal branch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem labore modi reprehenderit optio, omnis deserunt quae error cum iste molestiae, aperiam dolores? Aliquid sequi neque nemo in aspernatur asperiores sapiente tenetur totam, nisi beatae, quia quasi! Possimus vel nisi aut iure rerum repellendus, officiis odio dolor a? Doloribus, pariatur vero.</p>
 
   </div>
 </template>
